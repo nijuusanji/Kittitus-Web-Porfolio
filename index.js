0045a344fae722ed5941b959b382611172030f21
nijuusanji <57969861+nijuusanji@users.noreply.github.com>
2021-05-22 00:51:37 +0700

    well well
    welcome section done
    contact section done

--- index.js
+++ index.js
@@ -4,123 +4,127 @@ const illusImg = ["SB_1.jpg","SB_2.jpg","SB_3.jpg","SB_4.jpg","SB_5.jpg","SB_6.j
 var countPage = 1;
 
 function App() {
-
-    // const [imgIllus,setImgIllus] = React.useState(shotSrc+illusImg[0]);
-    const [imgIllus,setImgIllus] = React.useState(countPage);
-
-    const buttonIllus = (upOrDown) => {
-        if(upOrDown == 'Up' && countPage < illusImg.length && countPage>0){
-            countPage++;
-        }else if(upOrDown == 'Down' && countPage <= illusImg.length && countPage>1){
-            countPage--;
-        }else{
-            return;
-        }
-        setImgIllus(countPage)
-    }
-    
-    const helloThere = (index) => {
-        if(index == 1){
-            return shotSrc+illusImg[countPage-1]
-        }else if(index == 2){
-            return shotSrc+illusImg[countPage]
-        }else if(index == 3){
-            return shotSrc+illusImg[countPage+1]
-        }
-    }
+    const [aboutPage,setAboutPage] = React.useState(true);
+    const [worksPage,setWorksPage] = React.useState(false);
+    const [contactPage,setContactPage] = React.useState(false);
 
     return (    
     <div className="contain">
         <div className="navbar">
             <nav id="nav">
                 <div className="ui-text">
-                    <img src={shotSrc+"portUi.png"} alt="top" ></img>
-                    <p>Portfolio</p>
+                    <div className="set-ui-position">
+                        <img src={"./assets/images/portUi.png"} alt="top" ></img>
+                        <div id="titlePosition">
+                            <p id="titleShow">Welcome</p>
+                        </div>
+                    </div>  
                 </div>
                 <ul className="nav">
-                    <li><a href="#intro">Works</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a onClick={() => {setAboutPage(true);
+                                            setWorksPage(false)
+                                            setContactPage(false);
+                                            document.querySelector("#titleShow").innerText = 'Welcome';}}>About</a></li>
+                    <li><a onClick={() => {setAboutPage(false);
+                                            setWorksPage(true)
+                                            setContactPage(false)
+                                            document.querySelector("#titleShow").innerText = 'Works';}}>Works</a></li>
+                    <li><a onClick={() => {setAboutPage(false);
+                                            setWorksPage(false)
+                                            setContactPage(true)
+                                            document.querySelector("#titleShow").innerText = 'Contact';}}>Contact</a></li>
                 </ul>
             </nav>      
         </div>
-        <div className="welcome-section" id="abot">
-            <div className="hello">
-                <div className="conA">
-                    <div className="uiwelcome">
-                    <div className="name-container">
-                        <div className="myname">Kittitus</div>
-                    </div>
-                    <div className="position">
-                        <div className="myPostion">Game and Web Developer</div>
-
-                    </div>
-                    </div>
-                    
-                </div>
-                <div className="conB">
-                    <div className="flip-inner">
-                        <div className="flipfront">
-                            <div className="textflip">                               
-                                FLIP ME
-                            </div>
-                            <img className="profile" alt="Profile" src="./assets/images/hatang3.png"></img>
-                        </div>
-                        <div className="flipback">
-                            <div className="textflipBack">                               
-                                <p className="titleflip">My Profile</p>
-                                <div className="infoflip">
-                                <p>Name     : Kittitus Kongprakan</p> 
-                                <p>Nickname : Hathum </p>
-                                <p>Birthday : 12 June 1998</p>
-                                <p>Age      : 23</p>
-                                <p>Education: Media Technology</p>
-                                <p>Game Development</p>
-                                <p>King Mongkut's University of Technology Thonburi</p>
-                                <p>GPA 3.04</p>
-                                </div>
-                            </div>
-                            <img className="profile" alt="Profile" src="./assets/images/infoBackground.png"></img>
-                        </div>
-                    </div>
-                </div>     
-            </div>   
-        </div>
-        <div className="work-section">
-            <div className="intro-project" id="intro">
-                <div className="title-container">
-                    <a className="Title">My Works</a>
-                </div>
-                <a className="bton unity-button">
-    
-                </a>
-                <a className="bton animate-button">
-
-                </a>
-                <a className="bton art-button">
-    
-                </a>
-                <a className="bton codepen-button waves-effect waves-light" href="https://codepen.io/hathum-kittitus/pens/public" target="_blank">
-    
-                </a>
-                <button className="bton fcc-button">
-    
-                </button>
-            </div>
-            <div className="illustrator">
-                <a className="titel-illus">Illustrator</a>
-                <div className="picture-container">
-                    <img id="imgIllusLast" className="imgIllus" src={helloThere(1)} alt="illus2" />
-                    <img id="imgIllusMain"className="imgIllus" src={helloThere(2)} alt="illus2" />
-                    <img id="imgIllusNext" className="imgIllus" src={helloThere(3)} alt="illus2" />
-                </div>
-                <button onClick={() => buttonIllus('Up')} className="up">Up</button>
-                <button onClick={() => buttonIllus('Down')} className="down">Down</button>
-                <span className="numPage">Page {countPage} / 7</span>
-            </div>
-        </div>
+        
+            {aboutPage && !worksPage && !contactPage ? <Welcome /> : 
+            !aboutPage && worksPage && !contactPage? <Works /> : 
+            <Contact />}
     </div>
     )
 }
+
+function Contact(){
+    return (
+        <div className="contact-container">
+            <div className="tcb">
+                <div className="title-contact-bg">
+                    <img src="./assets/images/titlecontact.png" width="250px"alt="contactUi" />
+                    <p>Hire Me!</p>
+                </div>
+                <div className="contact-bg">
+                    
+                        <p>Tel. 086-600-2009</p>
+                        <p>Email. Kittitus.kong@outlook.com</p>
+                        <p>Facebook. Hathum Kittitus</p>
+                 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function Works(){
+    return (
+        <div className="work-container">
+            <div className="works-selector">
+                <div>Hello world</div>
+            </div>
+            <div className="work-side">
+
+            </div>
+        </div>
+    )
+}
+
+
+function Welcome(){
+    return (
+        <div className="welcome-section">
+    <div className="hello">
+    <div className="conA">
+        <div className="uiwelcome">
+        <div className="name-container">
+            <div className="myname">Kittitus</div>
+        </div>
+        <div className="position">
+            <div className="myPostion">Game and Web Developer</div>
+
+        </div>
+        </div>
+        
+    </div>
+    <div className="conB">
+        <div className="flip-inner">
+            <div className="flipfront">
+                <div className="textflip">                               
+                    FLIP ME
+                </div>
+                <img className="profile" alt="Profile" src="./assets/images/hatang3.png"></img>
+            </div>
+            <div className="flipback">
+                <div className="textflipBack">                               
+                    <p className="titleflip">My Profile</p>
+                    <div className="infoflip">
+                    <p>Name     : Kittitus Kongprakan</p> 
+                    <p>Nickname : Hathum </p>
+                    <p>Birthday : 12 June 1998</p>
+                    <p>Age      : 23</p>
+                    <p>Education: Media Technology</p>
+                    <p>Game Development</p>
+                    <p>King Mongkut's University of Technology Thonburi</p>
+                    <p>GPA 3.04</p>
+                    </div>
+                </div>
+                <img className="profile" alt="Profile" src="./assets/images/infoBackground.png"></img>
+            </div>
+        </div>
+    </div>     
+</div>
+</div>  )
+}
+
+
 
 
 ReactDOM.render( < App / > , document.getElementById("root"));
